6f18663bc60c564fe995e7d864f7bb273524a4f1
Neto Onyiaji <onyiajineto@gmail.com>
2017-03-14 17:20:21 +0100

    A simple command line that returns a twitter API

--- apps/commandlinelib.js
+++ apps/commandlinelib.js
@@ -1,23 +1,23 @@
-//A simple command line consumes a twitter API
 
 var Twitter = require('twitter');
  
 var client = new Twitter({
-  api_key:'T5op5tfnggjudlghhgugi',
-  api_secret:'DujlodjhnrudlsiihncXbvIFKwjWFKBlGeV16XEfsStb2DW9PC',
-  oauth_token:'42647953-r2kitodnope4hhghguyuyKudoplchtjultx9oprB',
-oAuth__token_secret:'t7utLubnhtboNcnSLaphtrkufiLeypphyy8zzhJTBNeZ' 
+  api_key:'U8dqeKAECXkURURfCBlrAhiMW',
+  api_secret:'Bh9w7mif2LcqyoZRmU7XbvIFKwjWFKBlGeV16XEfsStb2DW9PC',
+  oauth_token:'81451848-qZ8DIIN2p2F4dxl9BW9E57xpoOrpMx604Kx99QTYK',
+oauth__token_secret:'bl7WAeoUaNboNcnSLAcy4SFULdZ6ZO55M2aazzhJTBNeZ'
+
 
 });
 // using get 
- client.get('favorites', function(error, tweets, response) {
+ client.get('favorites/lists', function(error, tweets, response) {
   if(error) throw error;
   console.log(tweets);   
   console.log(response);   
 }); 
 
 //posting a tweet
-  client.post('update status', {status: 'Hello World!'},  function(error, tweet, response) {
+  client.post('status/update', {status: 'Hello World!'},  function(error, tweet, response) {
   if(error) throw error;
   console.log(tweet);   
   console.log(response);  
