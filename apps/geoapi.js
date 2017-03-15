36c9e77c97434bb9b8196328830c94b523756371
Onyiaji Netochukwu <onyiajineto@gmail.com>
2017-03-15 10:08:49 +0100

    geoapi

--- apps/geoapi.js
+++ apps/geoapi.js
@@ -1,12 +1,25 @@
-var googleMapsClient = require('@google/maps').createClient({
+#!/usr/bin/env node
+
+var program = require('commander');
+var googleMapsClient = require('@google/maps').createClient ({
   key: 'AIzaSyDYepC41G-B5tPe5NgKNNna-KtJx5YREo8'
 });
 
-// Geocode an address.
-googleMapsClient.geocode({
-  address: '1600 Amphitheatre Parkway, Mountain View, CA'
-}, function(err, response) {
-  if (!err) {
-    console.log(response.json.results);
-  }
-});
+program
+  .arguments('<add>')
+  .action(function(add){
+    googleMapsClient.geocode({
+      address: add
+      }, function (err, response){
+        if (!err){
+          var res = response.json.results;
+          var long = res[0].geometry.location.lng;
+          var lat = res[0].geometry.location.lat;
+          var fullAddress = res[0].formatted_address;
+          console.log("Full Address is: " + fullAddress);
+          console.log("Longitude is: " + long);
+          console.log("Latitude is: " + lat);
+        }
+      });
+  })
+  .parse(process.argv);
